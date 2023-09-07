aaa348cefda6198cf0ec7897d159d94b359f6fb0
laijy <faylai@gmail.com>
2023-09-07 17:37:47 +0800

    init push

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const fileUpload = require('express-fileupload');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 const app = express();
 // Parse incoming requests data (https://github.com/expressjs/body-parser)
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
+// default options
+app.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir: '/tmp/'
+}));
 const route = express.Router();
 
 const port = process.env.PORT || 5000;
@@ -20,19 +26,19 @@ const transporter = nodemailer.createTransport({
     port: 465,
     host: "smtp.gmail.com",
     auth: {
-        user: 'youremail@gmail.com',
-        pass: 'xxxxxxxxxx',
+        user: 'faylai@gmail.com',
+        pass: '',
     },
     secure: true, // upgrades later with STARTTLS -- change this based on the PORT
 });
 
 route.post('/text-mail', (req, res) => {
-    const {to, subject, text } = req.body;
+    const {to, subject, text} = req.body;
     const mailData = {
         from: 'youremail@gmail.com',
-        to: to,
-        subject: subject,
-        text: text,
+        to: 'faylai@qq.com',
+        subject: 'test',
+        text: 'a test',
         html: '<b>Hey there! </b><br> This is our first message sent with Nodemailer<br/>',
     };
 
@@ -40,35 +46,33 @@ route.post('/text-mail', (req, res) => {
         if (error) {
             return console.log(error);
         }
-        res.status(200).send({ message: "Mail send", message_id: info.messageId });
+        res.status(200).send({message: "Mail send", message_id: info.messageId});
     });
 });
 
 
 route.post('/attachments-mail', (req, res) => {
-    const {to, subject, text } = req.body;
+    const {to_user, subject, content} = req.body;
+    let attachments = Object.keys(req.files).map((key) => {
+        let file = req.files[key]
+        return {
+            filename: file.name,
+            path: file.tempFilePath
+        }
+    })
     const mailData = {
-        from: 'youremail@gmail.com',
-        to: to,
-        subject: subject,
-        text: text,
-        html: '<b>Hey there! </b><br> This is our first message sent with Nodemailer<br/>',
-        attachments: [
-            {   // file on disk as an attachment
-                filename: 'nodemailer.png',
-                path: 'nodemailer.png'
-            },
-            {   // file on disk as an attachment
-                filename: 'text_file.txt',
-                path: 'text_file.txt'
-            }
-        ]
+        from: 'service@acanva.com',
+        to: to_user || 'faylai@qq.com',
+        subject: subject || 'ACV+ Application',
+        html: content,
+        attachments: attachments
     };
 
     transporter.sendMail(mailData, (error, info) => {
         if (error) {
+            res.status(500).send({message: "mail send error see logs"});
             return console.log(error);
         }
-        res.status(200).send({ message: "Mail send", message_id: info.messageId });
+        res.status(200).send({message: "mail send", message_id: info.messageId});
     });
 });
